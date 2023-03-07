9a0cde947454a75931da47abfbe12a8dcabb9e01
Nrochester <123398837+Nrochester@users.noreply.github.com>
2023-03-07 13:01:36 -0500

    got all musicians

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = 3000;
 
 //TODO
 
+app.get("/musicians", async (request, response) => {
+    const musicians = await Musician.findAll();
+    response.json(musicians);
+})
+
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
