e8e653c91fca55e13f5bd472bb625b39816c8628
Nrochester <123398837+Nrochester@users.noreply.github.com>
2023-03-07 17:06:05 -0500

    Part 2

--- server.js
+++ server.js
@@ -6,11 +6,18 @@ const {sequelize} = require("./db")
 const port = 3000;
 
 //TODO
-
+/* PART 1
 app.get("/musicians", async (request, response) => {
     const musicians = await Musician.findAll();
     response.json(musicians);
 })
+*/
+
+/*PART 2*/
+app.get("/musicians/:id", async (request, response) => {
+    const music = request.params.id ;
+    response.json(await Musician.findByPk(music));
+  });
 
 app.listen(port, () => {
     sequelize.sync();
